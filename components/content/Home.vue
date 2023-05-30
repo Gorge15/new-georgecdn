26cef078d80553a818f61ec0e8f72c2a6fa21582
George Daniel <37251093+Gorge15@users.noreply.github.com>
2023-05-30 11:57:45 +0300

    a

---
+++ components/content/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="homepage">
+        <div class="title">
+            <span>Welcome to my website</span>
+        </div>
+        <div class="subtitle">
+            <span>My name is George</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less" scoped>
+@import "~/assets/global.less";
+    .homepage {
+        // padding: 25px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .title {
+            width: 100%;
+            span {
+                font-size: 48px;
+            }
+        }
+        
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,78 @@
 <template>
-    <div class="home">
+    <div class="home" @onScroll="handleScroll">
         <Sidebar class="sidebar" />
-        
+        <component class="component" :is="currentComponent"></component>
         <Navigation />
     </div>
-
 </template>
 
 <script>
-    import Sidebar from '../components/Sidebar.vue';
-    import Navigation from '../components/Navigation.vue';
+import Sidebar from '../components/Sidebar.vue';
+import Navigation from '../components/Navigation.vue';
+import Home from '~/components/content/Home.vue';
 
-    export default {
-        name: 'Home',
-        components: {
-            Sidebar,
-            Navigation,
+export default {
+    name: 'Home',
+    components: {
+        Sidebar,
+        Navigation,
+    },
+
+    data() {
+        return {
+            currentComponent: Home,
+            currentPos: null,
         }
+    },
+
+    methods: {
+        handleScroll(event) {
+            // Your scroll handling here
+            let delta = event.deltaY;
+            if (delta < 0) {
+                console.log('scroll up');
+
+            } else {
+                console.log('scroll down');
+            }
+        }
+    },
+
+    mounted() {
+
+    },
+
+    beforeMount() {
+        window.addEventListener('wheel', this.handleScroll)
+    },
+    beforeDestroy() {
+        window.removeEventListener('wheel', this.handleScroll)
     }
+}
 
 </script>
 
 <style lang="less">
-    .home {
-        width: 100%;
-        height: 100svh;
-        box-sizing: border-box;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .sidebar {
-            width: 450px;
-            height: 100%;
-            padding: 65px 25px;
-            
-        }
+.home {
+    width: 100%;
+    height: 100svh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .sidebar {
+        width: 450px;
+        height: 100%;
+        padding: 65px 25px;
+
     }
+
+    .component {
+        width: 70%;
+        height: 100%;
+        padding: 65px 25px;
+        border: 1px solid red;
+    }
+}
 </style>
